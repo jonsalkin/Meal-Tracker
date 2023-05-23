313b637cb41ffc96ed9aa0dc0498af7228ffd9d3
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-22 23:37:47 -0400

    Initial commit.

---
+++ Meal Tracker/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Jon Salkin on 5/22/23.
+//
+
+import Foundation
+
+struct Food {
+    let name: String
+    let description: String
+    
+}
---
+++ Meal Tracker/Meal.swift
@@ -0,0 +1,15 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Jon Salkin on 5/22/23.
+//
+
+import Foundation
+
+struct Meal {
+    let name: String
+    let food: [Food]
+    
+    
+}
