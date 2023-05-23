a747fe901882b9c17a4baf7cce4d96d6372dc6e9
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-22 22:58:00 -0400

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Jon Salkin on 5/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
